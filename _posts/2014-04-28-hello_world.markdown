953110a191350866c013e78fe8faf3d6ef4b6725
daodao10 <bebest10@gmail.com>
2014-04-28 02:48:30 +0800

    jekyll

---
+++ _posts/2014-04-28-hello_world.markdown
@@ -0,0 +1,7 @@
+	
+	# YAML Front Matter
+	---
+	layout: post
+	title: Hello World
+	date: 2014-04-28  2:11:43
+	---
---
+++ rakefile.rb
@@ -0,0 +1,32 @@
+desc "Given a title as an argument, create a new post file"
+task :write, [:title, :category] do |t, args|
+  filename = "#{Time.now.strftime('%Y-%m-%d')}-#{args.title.gsub(/\s/, '_').downcase}.md"
+  path = File.join("_posts", filename)
+  if File.exist? path; raise RuntimeError.new("Won't clobber #{path}"); end
+  File.open(path, 'w') do |file|
+    file.write <<-EOS
+---
+layout: post
+title: #{args.title}
+category: #{args.category}
+tags:
+-
+date: #{Time.now.strftime('%Y-%m-%d %k:%M:%S')}
+---
+EOS
+    end
+    puts "Now open #{path} in an editor."
+end
+
+desc 'Running Jekyll with --server --auto opition'
+task :run do
+  system('jekyll serve')
+end
+
+desc 'Upload my jekyll site to github'
+task :dev do
+  #system('jekyll')
+  system('git add .')
+  system("git commit -am 'jekyll'")
+  system('git push jekyll')
+end
